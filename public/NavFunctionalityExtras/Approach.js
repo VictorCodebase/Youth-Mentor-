bd89bbc17a2a7e14069be6d4430bc7b91bfef148
VictorKithinji <victorkithinji@outlook.com>
2023-06-02 08:23:40 +0300

    first commit

--- public/NavFunctionalityExtras/Approach.js
+++
@@ -1,5 +0,0 @@
-ApproachID = document.getElementById("Approach");
-ApproachText = document.getElementById("ApproachTxt");
-
-ApproachID.style.backgroundColor = "#64766A";
-ApproachText.style.color = "rgba(245, 245, 245, 0.78)";
--- public/NavFunctionalityExtras/Home.js
+++
@@ -1,5 +0,0 @@
-HomeID = document.getElementById("Home");
-HomeText = document.getElementById("HomeTxt");
-
-HomeID.style.backgroundColor = "#64766A";
-HomeText.style.color = "rgba(245, 245, 245, 0.78)"
